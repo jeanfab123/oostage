82537751925e34833e5fdf792fbea13f6e9d0356
jeanfab123 <jfabulas@gmail.com>
2018-09-25 17:57:17 +0200

    Création du formulaire de recherche, modifs CSS et HTML

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class SiteController extends AbstractController
 {
@@ -12,8 +14,34 @@ class SiteController extends AbstractController
      */
     public function index()
     {
+        $formSearch = $this->createFormBuilder()
+                           ->add('specialisation', TextType::class, [
+                               'attr' => [
+                                   'placeholder'    => "Exemple : PHP Vue.js Node.js",
+                                   'class'          => "form-control-lg"
+                               ],
+                               'label'     => false,
+                               'required'  => false,
+                           ])
+                           ->add('location', TextType::class, [
+                                'attr' => [
+                                    'placeholder'   => "Région, Ville",
+                                    'class'          => "form-control-lg"
+                                ],
+                                'label'     => false,
+                                'required'  => false,
+                            ])
+                            ->add('search', SubmitType::class, [
+                                    'label'         => "Rechercher",
+                                    'attr' => [
+                                        'class'         => "btn-lg"
+                                    ]
+                            ]);
+        $form = $formSearch->getForm();
+
         return $this->render('site/index.html.twig', [
-            'controller_name' => 'SiteController',
+            'controller_name'   => 'SiteController',
+            'formSearch'        => $form->createView()
         ]);
     }
 }
